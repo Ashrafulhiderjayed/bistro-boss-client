c619ae88dec0aa4d25b00db01ef825f365d80c91
Ashrafulhiderjayed <ashrafulhider87@gmail.com>
2023-05-25 09:32:43 +0600

    Banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,11 +3,13 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
             <Navbar></Navbar>
+            <Banner></Banner>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
